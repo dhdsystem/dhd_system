38f2c9e511bfda938ba50c998bf9a86639ab7e62
shanzezhao <774330595@qq.com>
2018-04-16 11:49:15 +0800

    shanzezhao
    common

--- Application/Home/Controller/CommonController.class.php
+++ Application/Home/Controller/CommonController.class.php
@@ -6,4 +6,17 @@ class CommonController extends Controller {
     public function common_index(){
        $this->display();
     }
+    // 无限极循环展示
+    public function GetTree($arr,$pid,$step){
+        global $tree;
+        foreach($arr as $key=>$val) {
+            if($val['pid'] == $pid) {
+                $flg = str_repeat('|__',$step);
+                $val['name'] = $flg.$val['name'];
+                $tree[] = $val;
+                $this->GetTree($arr , $val['id'] ,$step+1);
+            }
+        }
+        return $tree;
+    }
 }
