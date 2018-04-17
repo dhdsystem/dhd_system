198d31e4f596766ba96bdfe47ca2a7412ffe3ce7
shanzezhao <774330595@qq.com>
2018-04-17 14:31:19 +0800

    shanzezhao
    role diction

--- Application/Home/Controller/DictionController.class.php
+++ Application/Home/Controller/DictionController.class.php
@@ -1,9 +1,83 @@
 <?php
+/*shanzezhao*/
 namespace Home\Controller;
 use Think\Controller;
-class DictionController extends Controller {
-	/*权限*/
-    public function diction_index(){
- 		$this->display(); 
+class DictionController extends CommonController {
+	/*权限展示*/
+    public function diction_index(){          
+            $node = M('Node')->where('state = 1')->select();
+            $nodetree=$this->GetTrees($node,0,0);
+            // print_r($nodetree);die;
+            $this->assign('node',$nodetree);
+            return $this->display();
+ 		
     }
+    /*添加权限*/
+    public function diction_add(){
+        if(IS_POST){
+            $data=I('post.');
+            $res=M('Node')->data($data)->add();
+            if($res){
+                echo '1';
+            }else{
+                echo '0';
+            }
+            }else{
+        	$node = M('Node')->select();
+            $nodetree=$this->GetTrees($node,0,0);
+            $this->assign('node',$nodetree);
+            return $this->display();
+        }
+
+        }
+
+    /*删除*/
+        public function cdel(){
+            $id=I('post.id');
+            $data=M('node')->where(array('p_id'=>$id))->find();
+            if($data){
+                echo 3;die;
+            }
+            $res=M('node')->where(array('id'=>$id))->save(array('state'=>2));
+            if($res){
+                echo 1;
+            }else{
+                echo 2;
+            }
+        }
+    /*按权限名称搜索*/
+        public function diction_search(){
+              $name=I('get.name');
+              $node = M('Node');
+              $data=$node->where("state = 1 and opera_name like '%$name%'")->select();
+              //echo $node->getLastSql();
+              //print_r($node);die;
+              $this->assign('node',$data);
+              $this->display('diction_index');
+        }
+    /*修改*/
+        public function diction_save(){
+            if(IS_POST){
+                $data=I('post.');
+                //print_r(json_encode($data));die;
+                 $id=I('post.id');
+                 $res=M('Node')->where( array('id'=>$id))->save($data);
+                 if($res){
+                    echo 1;
+                 }else{
+                    echo 2;
+                 }
+            }else{
+                $id=I('get.id');
+                // print_r($id);die;
+                $data=M('Node')->where('id='.$id)->find();
+                //print_r($data);die;
+                /*权限名称展示*/
+                $node = M('Node')->select();
+                $nodetree=$this->GetTrees($node,0,0);
+                $this->assign('node',$nodetree);
+                $this->assign('data',$data);
+                return $this->display();
+            }
+        }
 }
--- Application/Home/Controller/RoleController.class.php
+++ Application/Home/Controller/RoleController.class.php
@@ -1,15 +1,71 @@
 <?php
+/*shanzezhao*/
 namespace Home\Controller;
 use Think\Controller;
-class RoleController extends Controller {
-	/*职业 角色*/
-    public function role_index(){
-       $this->display();
-    }
-     /*添加角色*/
-    public function  user_adas(){
-    
+class RoleController extends CommonController {
 
-        echo"2222";
+    /*职业 角色 展示*/
+    public function role_index(){
+        $role = M('Role')->where(array('state'=>1))->select();
+        $roletree=$this->GetTree($role,0,0);
+        //print_r($roletree);die;
+        $this->assign('role',$roletree);
+        $this->display();
+    }
+    /*添加角色*/
+    public function  role_add()
+    {
+        if(IS_POST){
+            //print_r($_POST);die;
+            $roledata['role_name']=I('post.role_name','');
+            $roledata['description']=I('post.description','');
+            $roledata['dtime']=time();
+            $roledata['state']='1';
+            $roledata['pid']=I('post.pid');
+            $res=M('role')->data($roledata)->add();
+            if(!$res){
+                return $this->error(json_encode('角色创建失败'));
+            }
+            $operalist=I('one','');//获取角色下的权限
+            foreach ($operalist as $key => $val) {
+                $arr[$key]['o_id']=$val;
+                $arr[$key]['r_id']=$res;
+                $arr[$key]['time']=time();
+
+            }
+            $re=M('role_node')->addAll($arr);
+
+            if(!$re){
+
+                 $this->error('角色创建失败');
+            }
+             $this->success('角色创建成功',U('role/role_index'));
+        }else{
+            $role = M('Role')->select();
+            $node = M('Node')->select();
+            $roletree=$this->GetTree($role,0,0);
+            $node=$this->get_attr($node);
+            $data=json_encode($node);
+            $this->assign('role',$roletree);
+            $this->assign('volist',$data);
+            $this->display('role_add');
+        }
+
+    }
+
+    /*删除*/
+    public function roledel(){
+        $id=I('post.id');
+        $res=M('role')->where(array('id'=>$id))->save(array('state'=>2));
+        if($res){
+            echo 1;
+        }else{
+            echo 2;
+        }
+    }
+
+    /*修改展示*/
+    public function  roleedit(){
+
     }
 }
