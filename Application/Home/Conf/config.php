39274bf1849ad50c90d336e2877eaee5f7e3e9b5
shiyan <15075892983@163.com>
2018-04-17 18:06:35 +0800

    史炎

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -2,8 +2,8 @@
 return array(
 	'URL_ROUTER_ON' => true,								//开启路由功能
 	'URL_CASE_INSENSITIVE' =>true,
-	'TMPL_L_DELIM' => '<{',									//修改左右定界符
-	'TMPL_R_DELIM' => '}>',									//修改左右定界符
+	// 'TMPL_L_DELIM' => '{',									//修改左右定界符
+	// 'TMPL_R_DELIM' => '}',									//修改左右定界符
 	'DEFAULT_CHARSET'=> 'utf-8',							//编码设置
 	'SESSION_AUTO_START' => true,							//开启SESSION
 	'URL_HTML_SUFFIX' => 'html|shmtl|xml', 					// 多个用 | 分割伪静态配置
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,31 +4,16 @@ use Think\Controller;
 class IndexController extends Controller {
 	/*首页*/
     public function index(){
-
-    	// echo session('username');
-  //   	$role_id = get_role_id();
-  //   	// echo $role_id;
-  //   	$where['p_id']=0;
-		// 	if($role_id!=1){
-		// 		$where['r_id']=$role_id;
-		// 	}
-
-  //   	$listmain=M('role_opera')->join('tp_compet on tp_role_opera.o_id=tp_compet.id','right')->where($where)->group('tp_compet.id')->order('grade desc')->select();
-  //   	// echo M('role_opera')->GetLastSql();
-		// //二级栏目
-		// foreach($listmain as $key=>$vo){
-		// 		$where['p_id']=$vo['id'];
-		// 	    $volists=M('role_opera')->join('tp_compet on tp_role_opera.o_id=tp_compet.id','right')->where($where)->group('tp_compet.id')->order('grade desc')->select();
-
-		// 		// $volists=$module->where('p_id ='.$vo['id'])->order('grade asc')->select();
-		// 		$volist[]=$volists;
+    	$listmain=M('node')->where(array('p_id'=>0))->select();
+		//二级栏目
+		foreach($listmain as $key=>$vo){
+				$where['p_id']=$vo['id'];
+				$volists=M('node')->where($where)->select();
+				$listmain[$key]['san']=$volists;
 			
-		// }
-		// $res = M('Contractinfo')->where('con_state=2')->count();
-		// $this->assign('count', $res);
-		// $this->assign('list', $listmain);
-		// $this->assign('volist', $volist);
+		}
+		// print_r($listmain);die;
+		$this->assign('list',$listmain);
     	$this->display();
-
     }
 }
