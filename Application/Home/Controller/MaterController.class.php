680817652e7250bef915d3a7531ab96af2428d8b
shanzezhao <774330595@qq.com>
2018-04-18 14:28:21 +0800

    单泽钊
    材料 缺少修改 缺少已用分数

--- Application/Home/Controller/MaterController.class.php
+++ Application/Home/Controller/MaterController.class.php
@@ -4,6 +4,54 @@ use Think\Controller;
 class MaterController extends Controller {
    /*材料*/
     public function mater_index(){
+       $where=array('m_type'=>0);
+       $page=pagess('material',$where,'',10);
+       $data=M('material')->alias('m')
+             ->join('dhd_class as c on c.id=m.class_id')
+             ->field('m.id,m_address,c.class_name,m_time,m_number')
+             ->limit($page['firstRow'],$page['listRows'])
+             ->order('m.id asc')
+             ->select();
+      //print_r($page);
+      //  print_r($data);die;
+       $this->assign('show',$page['show']);
+       $this->assign('data',$data);
        $this->display();
     }
+    /*材料添加*/
+	public function mater_add(){
+		if(IS_POST){
+			$data=I('post.');
+			$data['m_time']=time();
+			$res=M('material')->add($data);
+			if($res){
+				$this->success('材料添加成功',U('mater/mater_index'));
+			}else{
+				$this->error('材料添加失败',U('mater/mater_add'));
+			}
+		}else{
+			$class=M('Class')->field('id,class_name')->select();
+			$this->assign('class',$class);
+			$this->display();
+		}
+		
+	}
+	/*获取注册地址*/
+	public function mater_address(){
+		$id=I('post.class_id');
+		$data=M('details')->where(array('class_id'=>$id))->field('id,det_address')->select();
+		// print_r($data);die;
+		echo json_encode($data);
+	}
+	/*材料删除*/
+	public function mater_del(){
+		$id=I('post.id');
+		$res=M('material')->where(array('id'=>$id))->save(array('m_type'=>1));
+		if($res){
+			echo 1;
+		}else{
+			echo 2;
+		}
+	}
+
 }
