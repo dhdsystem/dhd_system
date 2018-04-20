d3b5af43d2883bff1c83d53c73decaa45ca193d0
shiyan <15075892983@163.com>
2018-04-20 10:25:54 +0800

    史炎

---
+++ Application/Home/Controller/ChannelController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ChannelController extends Controller {
+   /*渠道商*/
+    public function channel_index(){
+       $this->display();
+    }
+   	public function channel_add()
+   	{
+   		$this->display();
+   	}
+   	public function channeladd_do()
+   	{
+   		# code...
+   	}
+}
